5e86329000f01fdcc9a1bf02c8907cc0d070549f
Serhii Nosulia <sergeynosylya@gmail.com>
2023-11-10 18:28:25 +0200

    Added Book entity, BookRepository and BookService

---
+++ src/main/java/mate/bookshopapp/service/impl/BookServiceImpl.java
@@ -0,0 +1,28 @@
+package mate.bookshopapp.service.impl;
+
+import java.util.List;
+import mate.bookshopapp.model.Book;
+import mate.bookshopapp.repository.BookRepository;
+import mate.bookshopapp.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
