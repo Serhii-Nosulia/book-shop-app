1cc2d8904a8c166302b1b6cb16a6be0be60ca9cf
Serhii Nosulia <sergeynosylya@gmail.com>
2023-11-29 23:37:58 +0200

    added CustomGlobalExceptionHandler

--- src/main/java/mate/bookshopapp/dto/book/CreateBookRequestDto.java
+++ src/main/java/mate/bookshopapp/dto/book/CreateBookRequestDto.java
@@ -1,11 +1,13 @@
 package mate.bookshopapp.dto.book;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
-public record CreateBookRequestDto(String title,
-                                   String author,
-                                   String isbn,
-                                   BigDecimal price,
+public record CreateBookRequestDto(@NotNull String title,
+                                   @NotNull String author,
+                                   @NotNull String isbn,
+                                   @NotNull @Min(0) BigDecimal price,
                                    String description,
                                    String coverImage) {
 
--- src/main/java/mate/bookshopapp/dto/book/UpdateBookDto.java
+++ src/main/java/mate/bookshopapp/dto/book/UpdateBookDto.java
@@ -1,11 +1,12 @@
 package mate.bookshopapp.dto.book;
 
+import jakarta.validation.constraints.Min;
 import java.math.BigDecimal;
 
 public record UpdateBookDto(String title,
                             String author,
                             String isbn,
-                            BigDecimal price,
+                            @Min(0) BigDecimal price,
                             String description,
                             String coverImage) {
 }
