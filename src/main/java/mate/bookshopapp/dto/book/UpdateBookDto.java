3a9706a69258f89384e57ab7d2324f52e61ec605
Serhii Nosulia <sergeynosylya@gmail.com>
2023-12-01 14:27:31 +0200

    added @NotBlank annotation to title and author fields

--- src/main/java/mate/bookshopapp/dto/book/UpdateBookDto.java
+++ src/main/java/mate/bookshopapp/dto/book/UpdateBookDto.java
@@ -2,14 +2,15 @@ package mate.bookshopapp.dto.book;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import mate.bookshopapp.validation.Isbn;
 
-public record UpdateBookDto(@NotNull String title,
-                            @NotNull String author,
+public record UpdateBookDto(@NotBlank String title,
+                            @NotBlank String author,
                             @Isbn String isbn,
-                            @Min(0) BigDecimal price,
+                            @NotNull @Min(0) BigDecimal price,
                             @Max(9000) String description,
                             String coverImage) {
 }
